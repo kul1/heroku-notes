4ec721a0cc0026f0408330cb723fe5659e1022f7
Prateep Kul <1.0@kul.asia>
2020-02-08 23:50:42 -0600

    Re-Init with heroku version

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 class Address
   include Mongoid::Document
-		# jinda begin
-		include Mongoid::Timestamps
+  # jinda begin
+  include Mongoid::Timestamps
   field :address_street, :type => String
   field :city, :type => String
   field :state, :type => String
@@ -12,13 +12,3 @@ class Address
   field :lng, :type => Float
   # jinda end
 end
-		# jinda begin
-		include Mongoid::Timestamps
-  field :address_street, :type => String
-  field :city, :type => String
-  field :state, :type => String
-  field :zip, :type => String
-  field :phone, :type => String
-  field :lat, :type => Float
-  field :lng, :type => Float
-  # jinda end
